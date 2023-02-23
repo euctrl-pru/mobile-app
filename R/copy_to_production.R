d752f81f9ddeffa9a113c6511b98e873bfc592fe
OscarAlfaro73 <oscar.alfaro@eurocontrol.int>
2023-02-23 10:56:34 +0100

    added copy images folder to script

--- R/copy_to_production.R
+++ R/copy_to_production.R
@@ -12,5 +12,6 @@ destination_dir <- '//ihx-vdm05/LIVE_var_www_Economics$/DailyBriefing/Prototype2
 
 
 file.copy(file.path(base_dir,list.files(base_dir)), destination_dir, overwrite = TRUE)
-copyDirectory(here::here("iframes"), paste0(destination_dir,"iframes"))
-copyDirectory(here::here("data"), paste0(destination_dir,"data"))
+copyDirectory(here::here("iframes"), paste0(destination_dir,"iframes"), overwrite = TRUE)
+copyDirectory(here::here("data"), paste0(destination_dir,"data"), overwrite = TRUE)
+copyDirectory(here::here("images"), paste0(destination_dir,"images"), overwrite = TRUE)
