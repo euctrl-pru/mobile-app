9df80896a15470283e6bff9ed20f0087bc414a80
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2024-10-31 21:09:48 +0100

    paramatrised stakeholder

--- R/generate_past_data.R
+++ R/generate_past_data.R
@@ -11,12 +11,13 @@ source(here("..", "mobile-app", "R", "nw_queries.R"))
 
 test_archive_dir <- '//sky.corp.eurocontrol.int/DFSRoot/Groups/HQ/dgof-pru/Project/DDP/AIU app/data_archive'
 # set period
-wef <- "2024-09-02"  #included in output
-til <- "2024-10-27"  #included in output
+wef <- "2024-10-23"  #included in output
+til <- "2024-10-24"  #included in output
 
-myquery_string <- "query_nw_ao_y2d_raw" # set the name of the query function here
+myquery_string <- "query_ao_st_des_data_week_raw" # set the name of the query function here
 myarchivefile <- paste0(str_replace(myquery_string, "query", ""), ".csv")
-stakeholder <- stringr::str_sub(myarchivefile, 2,3)
+stakeholder <- stringr::str_sub(myarchivefile, 2,
+                                regexpr("_", substr(myarchivefile, 2, nchar(myarchivefile))))
 myquery <- function(mydate_string) {
   get(myquery_string)(mydate_string)
 }
