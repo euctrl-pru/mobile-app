a7b359576e9669aed2a0d031dea3d1d84bc791ba
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2025-01-27 17:28:08 +0100

    aligned archive script with dev version

--- R/generate_past_archive_data.R
+++ R/generate_past_archive_data.R
@@ -13,11 +13,17 @@ if(exists("mydate") == FALSE) {mydate <- "2024-01-01"}
 source(here("..", "mobile-app", "R", "queries_ap.R"))
 
 test_archive_dir <- '//sky.corp.eurocontrol.int/DFSRoot/Groups/HQ/dgof-pru/Project/DDP/AIU app/data_archive'
-# set period
-wef <- "2024-01-01"  #included in output
-til <- "2024-12-13"  #included in output
+# files_to_rename <- list.files(here(test_archive_dir, "ap"),
+#                               pattern = "ap_ap_des_data_week_raw",
+#                               full.names = TRUE)
+# new_filenames <- gsub("ap_ap_des_data_week_raw", "ap_ap_des_data_y2d_raw", files_to_rename)
+# file.rename(files_to_rename, new_filenames)
 
-myquery_string <- "query_ap_st_des_data_day_raw" # set the name of the query function here
+# set period
+wef <- "2024-11-10"  #included in output
+til <- "2025-01-20"  #included in output
+
+myquery_string <- "query_ap_ms_data_y2d_raw" # set the name of the query function here
 myarchivefile <- paste0(str_replace(myquery_string, "query", ""), ".csv")
 stakeholder <- stringr::str_sub(myarchivefile, 2,
                                 regexpr("_", substr(myarchivefile, 2, nchar(myarchivefile))))
@@ -28,14 +34,14 @@ myquery <- function(mydate_string) {
 cucu <- function(mydate_string) {
   mydate <- as.Date(mydate_string)
   mydate_prefix <-format(mydate, "%Y%m%d")
-#  myquery <- query_state_ao_day(mydate_string)
+  #  myquery <- query_state_ao_day(mydate_string)
   df <- export_query(myquery(mydate_string)) %>%
     as_tibble() %>%
     mutate(across(.cols = where(is.instant), ~ as.Date(.x)))
 
   write_csv(df,
             here(test_archive_dir, stakeholder, paste0(mydate_prefix, myarchivefile))
-            )
+  )
 }
 
 seq(ymd(wef), ymd(til), by = "day") |>
