6c9190b2254aa1807d0aefae7750b0aea0adaf89
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2026-04-13 06:35:45 +0000

    added bat file

---
+++ update_base_tables.bat
@@ -0,0 +1,3 @@
+:: file: C:\Users\oaolive\repos\mobile-app\run_mobile_app_update.bat
+@echo off
+"C:\Program Files\Git\bin\bash.exe" -lc "cd /c/Users/oaolive/repos/mobile-app && ./run_mobile_app_update.sh"
