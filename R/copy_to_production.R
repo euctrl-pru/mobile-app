d8e488687ac49bf0d7c3d4a09e0aa64a5c7360be
NCeuro-2024 <nora.cashman@eurocontrol.int>
2026-08-07 14:18:49 +0200

    adding data ready file

--- R/copy_to_production.R
+++ R/copy_to_production.R
@@ -23,8 +23,8 @@ network_data_folder_prod <- here(destination_dir, "data", "v5")
 archive_mode <- FALSE
 
 if (archive_mode) {
-  wef <- "2026-06-05" #included in output
-  til <- "2026-06-06" #included in output
+  wef <- "2026-08-03" #included in output
+  til <- "2026-08-05" #included in output
   data_day_date <- seq(ymd(wef), ymd(til), by = "day")
 } else {
   # wef <- "2026-07-09" #included in output
@@ -182,6 +182,15 @@ copy_app_data <- function(data_day_date) {
     recursive = TRUE,
     overwrite = TRUE
   )
+  
+  #adding data ready file
+  data_day_text_dash <- data_day_date %>% format("%Y-%m-%d")
+  network_data_folder_prod_date <- here(
+    network_data_folder_prod,
+    data_day_text_dash
+  )
+  writeLines("[]", paste0(network_data_folder_prod_date,"/DATA_READY.json"))
+  print("DATA_READY file added to folder")
 }
 
 # Define a combined function
@@ -205,14 +214,6 @@ for (i in length(data_day_date_temp):1) {
 }
 
 
-#adding data ready file
-data_day_text_dash <- data_day_date %>% format("%Y-%m-%d")
-network_data_folder_prod_date <- here(
-  network_data_folder_prod,
-  data_day_text_dash
-)
-writeLines("[]", paste0(network_data_folder_prod_date,"/DATA_READY.json"))
-print("DATA_READY file added to folder")
 
 
 # send email ----
@@ -237,6 +238,7 @@ if (archive_mode == FALSE) {
     from = from,
     to = to,
     # cc = cc,
+    subject = sbj,
     msg = msg,
     control = control
   )
